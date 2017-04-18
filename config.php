ed9e7291fc2ad63d2e98c27eaaafd6471c07b79c
Paula Watt <pwatt9@users.noreply.github.com>
2017-04-18 16:05:23 -0500

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Note: this file did not generally go into source control in my last job.
-
-// MySQL/MariaDB 10.1.21
-$db_host = '127.0.0.1';
-$db_name = 'hotels';
-$db_user = 'quore';
-$db_pass = 'KYkcPpiwK59ucXjM';
-$db_charset = 'utf8mb4';
